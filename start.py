b9e737c8af2a3dddbae45a43620b6cb266411118
Rubaiyat Yameen <64032439+ryameen@users.noreply.github.com>
2022-03-15 03:37:28 -0400

    first commit

---
+++ start.py
@@ -0,0 +1,68 @@
+# age = 12
+# name = "Rubaiyat"
+# wife = "Rushda"
+# isHot = True
+# isRich = False
+# # Learn Python Data types
+# print(age)
+# print(name)
+# print(wife)
+# print(isHot)
+# print(isRich)
+# print(88+21)
+# print(161%3)
+
+# spider = 5
+# ironMan = 15
+# print(spider + ironMan)
+
+# firstName = "Justin"
+# lastName = "Timberlake"
+# print(firstName + lastName)
+# print(firstName + " " + lastName)
+
+# name = input("Provide your Name: ")
+
+# print(name)
+
+# # Learn About inputs
+# num1 = input("Provide first number: ")
+# num2 = input("Provide second number: ")
+# n1 = int(num1)
+# n2 = int(num2)
+
+# print(n1 + n2)
+
+# pri1 = input("Provide first price: ")
+# pri2 = input("Provide second price: ")
+# p1 = float(pri1)
+# p2 = float(pri2)
+
+# print(p1 + p2)
+
+# price = 490
+
+# if price > 500:
+#     print("I will not buy it")
+# else:
+#     print("I will give it as a gift")
+
+ages = [37, 42, 45, 36, 31]
+
+# for age in ages:
+#     print("This friend's age is ", age)
+
+# for num in range(0, 10):
+#     print(num)
+num = 0
+while num < 10:
+    print(num)
+    num = num + 1
+
+def brushTeeth(num):
+    print("Brushing")
+    return num * 2
+    
+
+brushTeeth(input("How many times to brush teeth? "))
+
